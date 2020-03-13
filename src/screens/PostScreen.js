8a2cb3c76fa96ab0d9f58d5ef667f37783d5e56e
VjacheslavZ <slavikov.net@gmail.com>
2020-03-13 13:38:21 +0200

    Implementation page Post

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
-export const PostScreen = ({}) => {
+export const PostScreen = ({ navigation }) => {
+  const postId = navigation.getParam('postId');
+
   return <View style={styles.center}>
-    <Text>PostScreen</Text>
+    <Text>{postId}</Text>
   </View>
 };
 
-PostScreen.navigationOptions = {
-  headerTitle: 'Post #xxx',
+PostScreen.navigationOptions = ({ navigation }) => {
+  const date = navigation.getParam('date');
+  return {
+    headerTitle: `Post ${new Date(date).toLocaleDateString()} `,
+  }
 };
 
 const styles = StyleSheet.create({
